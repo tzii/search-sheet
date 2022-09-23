da97ca939a760eab531da715f695331afa14f962
tzii <tuanvundvn@gmail.com>
2022-09-23 18:13:50 +0700

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import 'react-data-grid/lib/styles.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider>
+			<Head>
+				<title>Đông bán thuốc</title>
+				{/* <meta name="viewport" content="initial-scale=1.0, width=device-width" /> */}
+			</Head>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
